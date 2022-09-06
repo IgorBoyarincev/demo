a8ce3d5eef4622722597b79d4c7badac7c29774e
IgorBoyarincev <boyarincev.igor@list.ru>
2022-09-06 13:26:13 +0300

    checked type and commit on longer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {40, 15, 35, 12, 4, 88, 56, 77, 23, 66};
+        long[] sales = {40, 15, 35, 12, 4, 88, 56, 77, 23, 66};
         ManagerSales sale = new ManagerSales(sales);
-        int max = sale.max();
+        long max = sale.max();
         System.out.println(max);
     }
 }
